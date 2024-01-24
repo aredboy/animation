e46d8bc991d7d29d551b312a8089b0b09af75434
aredboy <germanrojo.biologistic@gmail.com>
2024-01-24 16:16:39 -0300

    volvimos

--- src/components/Animacion/Animacion.jsx
+++ src/components/Animacion/Animacion.jsx
@@ -1,9 +1,6 @@
 import './Animacion.css';
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from 'react';
-import ovNi from './public/Ovni.svg';
-import letras from './public/Letras.svg';
-import leaf from './public/leaf.svg';
 
 const Animacion = () => {
 
@@ -16,13 +13,13 @@ const Animacion = () => {
         initial={{opacity:0}} 
         animate={{opacity:1}} 
         transition={{delay: 6, duration: 3}}
-        src={letras} alt="nombre" className='nombre'
+        src="../public/Letras.svg" alt="nombre" className='nombre'
       />
 
       <motion.img
         animate={{y: [-400,50], x: [100,220], scale: 1}} 
         transition={{type:"spring",delay: 3, duration: 3, times: [0.1,.1, 0.4, 1]}}
-        src={ovNi}  alt="Ovni" className='logo'
+        src="../public/Ovni.svg"  alt="Ovni" className='logo'
       />
       <AnimatePresence>
         {isVisible && (   
@@ -31,7 +28,7 @@ const Animacion = () => {
           transition={{delay: 1,duration: 2,velocity: 1}}
           exit={{opacity:0, y:-500, duration:1}}
           onClick={()=>setIsVisible(!isVisible)}
-          src={leaf} alt="hoja" className='hoja' />
+          src="../public/leaf.svg" alt="hoja" className='hoja' />
         )}
       {!isVisible && (
       <motion.p 
