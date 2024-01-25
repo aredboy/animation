6a38cd13ef1196e2afb8a929024a6a32c8ac6c9e
aredboy <germanrojo.biologistic@gmail.com>
2024-01-24 22:43:15 -0300

    carpeta img

--- src/components/Animacion/Animacion.jsx
+++ src/components/Animacion/Animacion.jsx
@@ -13,13 +13,13 @@ const Animacion = () => {
         initial={{opacity:0}} 
         animate={{opacity:1}} 
         transition={{delay: 6, duration: 3}}
-        src="../public/Letras.svg" alt="nombre" className='nombre'
+        src="./img/Letras.svg" alt="nombre" className='nombre'
       />
 
       <motion.img
         animate={{y: [-400,50], x: [100,220], scale: 1}} 
         transition={{type:"spring",delay: 3, duration: 3, times: [0.1,.1, 0.4, 1]}}
-        src="../public/Ovni.svg"  alt="Ovni" className='logo'
+        src="./img/Ovni.svg"  alt="Ovni" className='logo'
       />
       <AnimatePresence>
         {isVisible && (   
@@ -28,7 +28,7 @@ const Animacion = () => {
           transition={{delay: 1,duration: 2,velocity: 1}}
           exit={{opacity:0, y:-500, duration:1}}
           onClick={()=>setIsVisible(!isVisible)}
-          src="../public/leaf.svg" alt="hoja" className='hoja' />
+          src="./img/leaf.svg" alt="hoja" className='hoja' />
         )}
       {!isVisible && (
       <motion.p 
